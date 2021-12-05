f7f86e34f3901bfeb8dddfac75a5d173413cc09f
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-12-05 09:40:01 +0200

    Refactor day 5

--- lib/vents.ex
+++ lib/vents.ex
@@ -1,26 +1,25 @@
 defmodule Aoc2021.Vents do
   def overlaps(coords) do
+    regex = ~r/(\d+),(\d+) -> (\d+),(\d+)/
+
     coords
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.split(&1, "->", trim: true))
+    |> Enum.map(fn line ->
+      Regex.run(regex, line, capture: :all_but_first)
+      |> Enum.map(&String.to_integer/1)
+      |> then(fn [x1, y1, x2, y2] -> [{x1, y1}, {x2, y2}] end)
+    end)
     |> Enum.flat_map(&expand/1)
     |> Enum.frequencies()
     |> Enum.count(fn {_key, val} -> val > 1 end)
   end
 
-  defp expand([a, b]) do
-    [x1, y1] = to_coord(a)
-    [x2, y2] = to_coord(b)
-
+  defp expand([{x1, y1}, {x2, y2}]) do
     cond do
       x1 == x2 -> for y <- y1..y2, do: {x1, y}
       y1 == y2 -> for x <- x1..x2, do: {x, y1}
       abs(y1 - y2) == abs(x1 - x2) -> Enum.zip(x1..x2, y1..y2)
       :otherwise -> []
     end
-  end
-
-  defp to_coord(val) do
-    Enum.map(String.split(val, ","), &(String.trim(&1) |> String.to_integer()))
   end
 end
