33c5ff9a7cfbb7b3cdd38cf4ad6650add282bb43
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-12-06 08:58:19 +0200

    Refactor day 6

--- lib/lantern_fish.ex
+++ lib/lantern_fish.ex
@@ -1,39 +1,30 @@
 defmodule Aoc2021.LaternFish do
   def growth(input) do
-    fish =
+    init =
       input
-      |> String.trim()
       |> String.split(",", trim: true)
       |> Enum.map(&String.to_integer/1)
       |> Enum.frequencies()
 
-    fish =
-      for count <- 0..8, into: %{} do
-        case fish[count] do
-          nil -> {count, 0}
-          val -> {count, val}
-        end
-      end
-
-    populate(fish, 0)
+    for(count <- 0..8, do: init[count] || 0)
+    |> List.to_tuple()
+    |> populate(0)
   end
 
-  defp populate(fish, days) when days >= 256, do: Enum.sum(Map.values(fish))
+  defp populate(fish, days) when days >= 256, do: Tuple.sum(fish)
 
   defp populate(fish, days) do
-    populate(
-      %{
-        0 => fish[1],
-        1 => fish[2],
-        2 => fish[3],
-        3 => fish[4],
-        4 => fish[5],
-        5 => fish[6],
-        6 => fish[7] + fish[0],
-        7 => fish[8],
-        8 => fish[0]
-      },
-      days + 1
-    )
+    {
+      elem(fish, 1),
+      elem(fish, 2),
+      elem(fish, 3),
+      elem(fish, 4),
+      elem(fish, 5),
+      elem(fish, 6),
+      elem(fish, 7) + elem(fish, 0),
+      elem(fish, 8),
+      elem(fish, 0)
+    }
+    |> populate(days + 1)
   end
 end
